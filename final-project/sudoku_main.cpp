cfdb3baf5a1affa1ee62754b9a09dec54cebb420
aa35037123 <wesleyshi0321@gmail.com>
2024-12-07 17:40:50 +0800

    Update:New 16 x 16 mazes

--- final-project/sudoku_main.cpp
+++ final-project/sudoku_main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
     // Dynamically allocate the Sudoku grid
     Sudoku sudoku;
     sudoku.loadSudoku(filename);
-
+    
     // Create a brute force serial solver object
     SerialBruteForceSolver* solver = new SerialBruteForceSolver(sudoku);
     solver->solve();
